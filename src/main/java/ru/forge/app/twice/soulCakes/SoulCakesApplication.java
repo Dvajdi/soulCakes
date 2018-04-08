208719b5deae1c301a58f751baddea437de99806
Twice <dvajdi@bk.ru>
2018-04-08 19:38:00 +0300

    start project

---
+++ src/main/java/ru/forge/app/twice/soulCakes/SoulCakesApplication.java
@@ -0,0 +1,12 @@
+package ru.forge.app.twice.soulCakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulCakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulCakesApplication.class, args);
+	}
+}
